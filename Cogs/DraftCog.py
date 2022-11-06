419d9365d27ae9e9b9961d0eb41fa72c1067788f
XriXis <xzsaythjpf@gmail.com>
2022-11-06 15:24:08 +0300

    Take 2 new variants of starting draft system

--- Cogs/DraftCog.py
+++ Cogs/DraftCog.py
@@ -1,7 +1,8 @@
-from discord import member
+from discord import member, Option, Member
 from discord.ext import commands
 
 from BRBot import BRBot
+from Utils.JsonHandler import settings
 from Utils.MessageLib import custom_embed
 from Utils.DraftSystem.Draft import Draft
 
@@ -14,15 +15,27 @@ class DraftCog(commands.Cog):
     def __init__(self, bot: BRBot) -> None:
         self.bot: BRBot = bot
 
-    @commands.user_command(name="draft with", description="you can throw fight to user")
-    async def draft(self, ctx, captain: member) -> None:
-        if captain == ctx.author:
+    @commands.has_role(settings["privileged role"])
+    @commands.slash_command(name="draft_between", description="start draft between 2 members of your guild")
+    async def owner_draft(self, ctx, captain1: Option(Member), captain2: Option(Member)):
+        await self.draft(ctx, captain1, captain2)
+
+    @commands.slash_command(name="draft_with", description="you can throw fight to the user")
+    async def text_draft(self, ctx, captain: Option(Member)):
+        await self.draft(ctx, ctx.author, captain)
+
+    @commands.user_command(name="draft_with", description="you can throw fight to user")
+    async def context_draft(self, ctx, captain: member) -> None:
+        await self.draft(ctx, ctx.author, captain)
+
+    async def draft(self, ctx, captain1: Member, captain2: Member):
+        if captain1 == captain2:
             await ctx.respond(embed=custom_embed(False, "1 capitan in draft"))
-        elif captain == self.bot.user:
+        elif any(captain == self.bot.user for captain in (captain1, captain2)):
             await ctx.respond(embed=custom_embed(False, "draft with bot"))
         else:
             await ctx.respond(embed=custom_embed(True, "empty", "👌"))
-            draft = Draft(ctx.author, captain, ctx.channel)
+            draft = Draft(captain1, captain2, ctx.channel)
             await draft.start()
 
 
