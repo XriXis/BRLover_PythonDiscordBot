2719f498f0dcbdd843d7586a6f8179b2c383007c
XriXis <xzsaythjpf@gmail.com>
2022-11-12 15:37:22 +0300

    Working with balance voice member

---
+++ Utils/HIdednLeague/Balance/Ui/SrangersSelectMenu.py
@@ -0,0 +1,33 @@
+from discord import SelectOption, Interaction
+from discord.ui import Select, View
+
+from Utils.JsonHandler import settings
+from Utils.MessageLib import custom_embed
+
+
+class StrangersSelectMenu(Select):
+    def __init__(self, clutch):
+        self.clutch = clutch
+        super().__init__(
+            placeholder="Choose play lvl of unknown player",
+            min_values=1,
+            max_values=1,
+            options=[
+                SelectOption(label=role, value=role)
+                for role in settings["league_roles"]
+            ]
+        )
+
+    async def callback(self, interaction: Interaction):
+        self.clutch.strangers.append(self.values[0])
+        if len(self.clutch.strangers) + len(self.clutch.members) != 6:
+            view = View()
+            self.placeholder = "Choose play lvl of unknown player, again"
+            view.add_item(self)
+            await interaction.response.edit_message(
+                embed=custom_embed(True, "chose again"),
+                view=view
+            )
+        else:
+            await self.clutch.start_balance()
+            await interaction.message.delete()
