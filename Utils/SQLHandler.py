f257fc2f6634ef776fed8dc3910920710eda541a
XriXis <xzsaythjpf@gmail.com>
2022-11-12 15:30:05 +0300

    My git broke, try to fix

--- Utils/SQLHandler.py
+++ Utils/SQLHandler.py
@@ -1,3 +1,15 @@
-# TO DO:
-# 1. Hidden league utils
-# 2. location variable
+class SQLHandler:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_score(member_id: int) -> int:
+        pass
+
+    @staticmethod
+    def update_score(name: str, diff_score: int) -> None:
+        pass
+
+    @staticmethod
+    def new_member(name: str, score: int) -> None:
+        pass
